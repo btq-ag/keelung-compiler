d1881c6a6eb5b77c3295b3749101a886580d6084
Luā Tîng-Giān <banacorn@gmail.com>
2022-03-17 07:35:33 +0000

    [ new ] Constraint & ConstraintSystem

--- src/Keelung/Constraint.hs
+++ src/Keelung/Constraint.hs
@@ -1,12 +1,69 @@
 module Keelung.Constraint where
 
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Keelung.Constraint.CoeffMap (CoeffMap)
 import Keelung.Syntax
+import Keelung.Util (DebugGF (DebugGF))
 
 --------------------------------------------------------------------------------
 
-data Constraint a
-  = CAdd !a !(CoeffMap a)
-  | CMult !(a, Var) !(a, Var) !(a, Maybe Var)
+-- | Constraint
+--      CAdd: 0 = c + c₀x₀ + c₁x₁ ... cₙxₙ
+--      CMult: nx + by = c or nx + by = cz
+data Constraint n
+  = CAdd !n !(CoeffMap n)
+  | CMult !(n, Var) !(n, Var) !(n, Maybe Var)
+
+instance (Show n, Eq n, Num n) => Show (Constraint n) where
+  show (CAdd 0 m) = show m
+  show (CAdd n m) = show n <> " + " <> show m
+  show (CMult (a, x) (b, y) (c, z)) =
+    let showTerm 1 var = "$" <> show var
+        showTerm coeff var = show coeff <> "$" <> show var
+     in showTerm a x <> " * " <> showTerm b y
+          <> " = "
+          <> case z of
+            Nothing -> show c
+            Just z' -> showTerm c z'
 
 --------------------------------------------------------------------------------
+
+-- | Constraint System
+data ConstraintSystem n = ConstraintSystem
+  { csConstraints :: Set (Constraint n),
+    csNumberOfVars :: Int,
+    csInputVars :: IntSet,
+    csOutputVars :: IntSet
+  }
+
+instance (Show n, Bounded n, Integral n, Fractional n) => Show (ConstraintSystem n) where
+  show (ConstraintSystem set numVars inputVars outputVars) =
+    "ConstraintSystem {\n\
+    \  number of constraints: "
+      <> show (Set.size set)
+      <> "\n"
+      <> ( if Set.size set < 20
+             then "  constraints:\n" <> printConstraints set <> "\n"
+             else ""
+         )
+      <> "  number of variables: "
+      <> show numVars
+      <> "\n\
+         \  number of input variables: "
+      <> show (IntSet.size inputVars)
+      <> "\n  output variables: "
+      <> show (IntSet.toList outputVars)
+      <> "\n\
+         \}"
+    where
+      printConstraint :: (Show n, Bounded n, Fractional n, Integral n) => Constraint n -> String
+      printConstraint (CAdd n xs) =
+        "    " <> show (CAdd (DebugGF n) (fmap DebugGF xs))
+      printConstraint (CMult (a, x) (b, y) (c, z)) =
+        "    " <> show (CMult (DebugGF a, x) (DebugGF b, y) (DebugGF c, z))
+
+      printConstraints :: (Show n, Bounded n, Fractional n, Integral n) => Set (Constraint n) -> String
+      printConstraints = unlines . map printConstraint . Set.toList
--- src/Keelung/Util.hs
+++ src/Keelung/Util.hs
@@ -1,12 +1,30 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Keelung.Util where
 
-import Data.Field.Galois (GaloisField)
+import Data.Euclidean (Field, Euclidean, GcdDomain)
+import Data.Semiring (Ring, Semiring)
 
 -- for ease of debugging really big GF181 numbers
 newtype DebugGF a = DebugGF {unDebugGF :: a}
   deriving (Eq, Ord)
 
-instance (Show f, GaloisField f, Bounded f, Integral f) => Show (DebugGF f) where
+-- deriving instance (Bounded n, Field n, GaloisField n) => GaloisField (DebugGF n)
+deriving instance Bounded n => Bounded (DebugGF n)
+deriving instance Field n => Field (DebugGF n)
+deriving instance Euclidean n => Euclidean (DebugGF n)
+deriving instance Ring n => Ring (DebugGF n)
+deriving instance GcdDomain n => GcdDomain (DebugGF n)
+deriving instance Semiring n => Semiring (DebugGF n)
+deriving instance Fractional n => Fractional (DebugGF n)
+deriving instance Num n => Num (DebugGF n)
+
+-- instance (GaloisField n, Bounded n, Fractional (DebugGF n)) => GaloisField (DebugGF n) where 
+--   char = char . unDebugGF
+--   deg = deg . unDebugGF
+--   frob = DebugGF . frob . unDebugGF
+
+instance (Show n, Bounded n, Integral n, Fractional n) => Show (DebugGF n) where
   show (DebugGF coeff) =
     let halfway = maxBound / 2
      in if coeff >= halfway
