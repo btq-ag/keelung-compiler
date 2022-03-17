00a4e183a169859c128eaa7a12d8ba4d8d833d6b
Luā Tîng-Giān <banacorn@gmail.com>
2022-03-17 10:12:05 +0000

    [ new ] Untyped expressions

---
+++ src/Keelung/Syntax/Untyped.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE GADTs #-}
+
+module Keelung.Syntax.Untyped where
+
+import Keelung.Syntax (Var)
+import qualified Keelung.Syntax as T
+
+--------------------------------------------------------------------------------
+
+-- Binary operators
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | And
+  | Or
+  | Xor
+  | Eq
+  deriving (Eq)
+
+-- See if an operator is associative, so that we can chain them together
+isAssoc :: Op -> Bool
+isAssoc op = case op of
+  Add -> True
+  Sub -> False
+  Mul -> True
+  Div -> False
+  And -> True
+  Or -> True
+  Xor -> True
+  Eq -> True
+
+--------------------------------------------------------------------------------
+
+-- | Untyped expression
+data Expr n
+  = Var Var
+  | Val n
+  | BinOp Op [Expr n]
+  | IfThenElse (Expr n) (Expr n) (Expr n)
+  deriving (Eq)
+
+--------------------------------------------------------------------------------
+
+eraseType :: Num n => T.Expr n ty -> Expr n
+eraseType expr = case expr of
+  T.Val val -> case val of
+    (T.Number n) -> Val n
+    (T.Boolean True) -> Val 1
+    (T.Boolean False) -> Val 0
+  T.Var (T.Variable n) -> Var n
+  T.Add x y -> chainExprs Add (eraseType x) (eraseType y)
+  T.Sub x y -> chainExprs Sub (eraseType x) (eraseType y)
+  T.Mul x y -> chainExprs Mul (eraseType x) (eraseType y)
+  T.Div x y -> chainExprs Div (eraseType x) (eraseType y)
+  T.Eq x y -> chainExprs Eq (eraseType x) (eraseType y)
+  T.And x y -> chainExprs And (eraseType x) (eraseType y)
+  T.Or x y -> chainExprs Or (eraseType x) (eraseType y)
+  T.Xor x y -> chainExprs Xor (eraseType x) (eraseType y)
+  T.BEq x y -> chainExprs Eq (eraseType x) (eraseType y)
+  T.IfThenElse b x y -> IfThenElse (eraseType b) (eraseType x) (eraseType y)
+
+-- Flatten and chain expressions together when possible
+chainExprs :: Op -> Expr n -> Expr n -> Expr n
+chainExprs op x y = case (x, y) of
+  (BinOp op1 xs, BinOp op2 ys)
+    | op1 == op2 && op2 == op && isAssoc op ->
+      -- chaining `op`, `op1`, and `op2`
+      BinOp op (xs ++ ys)
+  (BinOp op1 xs, _)
+    | op1 == op && isAssoc op ->
+      -- chaining `op` and `op1`
+      BinOp op (xs ++ [y])
+  (_, BinOp op2 ys)
+    | op2 == op && isAssoc op ->
+      -- chaining `op` and `op2`
+      BinOp op (x : ys)
+  -- there's nothing left we can do
+  _ -> BinOp op [x, y]
