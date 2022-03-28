8cad4b67cafe21e4f80cafd3f0b6a93aaafd062d
Luā Tîng-Giān <banacorn@gmail.com>
2022-03-28 13:16:20 +0000

    [ doc ] Benchmarking optimised Aggregate Signature

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RebindableSyntax #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use <&>" #-}
 
 module Basic where
 
-import AggregateSignature.Program.Keelung as Keelung 
+import qualified AggregateSignature.Program.Keelung as Keelung 
 import AggregateSignature.Util
-import Keelung
-import qualified Keelung.Optimiser.ConstantPropagation as ConstantPropagation
 import qualified Data.IntMap as IntMap
+import qualified Data.Set as Set
+import Keelung 
+import qualified Keelung.Optimiser.ConstantPropagation as ConstantPropagation
 
 --------------------------------------------------------------------------------
 
 constant1 :: Comp 'Num GF181
 constant1 = do
-  return $ 1 + 1 
+  return $ 1 + 1
 
 identity :: Comp 'Num GF181
 identity = Var <$> freshInput
@@ -28,11 +30,11 @@ add3 = do
   x <- freshInput
   return $ Var x + 3
 
--- takes an input and see if its equal to 3 
+-- takes an input and see if its equal to 3
 eq1 :: Comp 'Bool GF181
 eq1 = do
-  x <- freshInput 
-  return $ Var x `Eq` 3 
+  x <- freshInput
+  return $ Var x `Eq` 3
 
 cond :: Comp 'Num GF181
 cond = do
@@ -54,33 +56,25 @@ loop2 = do
   arr2 <- freshInputs 2
   arrayEq 2 arr (arr2 :: (Ref ('A ('V 'Num))))
 
-
-
 aggSig :: Int -> Int -> Comp 'Bool GF181
 aggSig dim num = do
-  let settings = Settings True False False False False
-  -- let settings = Settings True True True True True
+  let settings = Settings True True True True True
   let setup = makeSetup dim num 42 settings
+  Keelung.aggregateSignature setup
 
-
-  aggregateSignature setup
-
-q1 :: Comp 'Bool GF181
-q1 = aggSig 1 1
-
-q2 :: Comp 'Bool GF181
-q2 = aggSig 1 10
-
-q3 :: Comp 'Bool GF181
-q3 = aggSig 10 10
-
-checkSig :: Int -> Int -> Comp 'Bool GF181 
+checkSig :: Int -> Int -> Comp 'Bool GF181
 checkSig dimension n = do
   let settings = Settings True False False False False
   let Setup _ _ publicKey signatures _ _ = makeSetup dimension n 42 settings
   expectedAggSig <- freshInputs dimension
-  actualAggSig <- computeAggregateSignature publicKey signatures
+  actualAggSig <- Keelung.computeAggregateSignature publicKey signatures
   arrayEq dimension expectedAggSig actualAggSig
+
+-- checkBitBoolean :: Int -> Int -> [GF181] 
+-- checkBitBoolean dimension n =
+--   let settings = Settings False False True False False
+--       setup = makeSetup dimension n 42 settings
+--   in  genInputFromSetup setup
 
 checkSquares :: Int -> Int -> Comp 'Bool GF181
 checkSquares dimension n = do
@@ -89,28 +83,74 @@ checkSquares dimension n = do
   sigSquares <- freshInputs2 n dimension
   Keelung.checkSquares n dimension signatures sigSquares
 
+checkSigSize :: Int -> Int -> Comp 'Bool GF181
+checkSigSize dimension n = do
+  let settings = Settings False False False True False
+  let Setup _ _ _ signatures _ _ = makeSetup dimension n 42 settings
+  sigBitStrings <- freshInputs3 n dimension 14
+
+  
+  Keelung.checkSignaturesBitStringSize dimension signatures sigBitStrings
+
+checkSigLength :: Int -> Int -> Comp 'Bool GF181
+checkSigLength dimension n = do
+  sigSquares <- freshInputs2 n dimension
+  sigLengths <- freshInputs n
+  Keelung.checkLength n dimension sigSquares sigLengths
 
 --------------------------------------------------------------------------------
 
+bench :: Comp 'Bool GF181 -> Settings -> Int -> Int -> Either String (Int, Int, Int)
+bench program settings dimension n = do 
+  let input = genInputFromSetup (makeSetup dimension n 42 settings)
+  cs <- comp program
+  cs' <- optm program
+  (_, cs'') <- optmWithInput program input
+  return (Set.size (csConstraints cs), Set.size (csConstraints cs'), Set.size (csConstraints cs''))
+      
+-- #1
+runCheckSig :: Int -> Int -> Either String (Int, Int, Int)
+runCheckSig dimension n = do 
+  let settings = Settings True False False False False
+  bench (checkSig dimension n) settings dimension n
 
--- elaborate & erase type 
+-- #2
+runCheckSigSize :: Int -> Int -> Either String (Int, Int, Int)
+runCheckSigSize dimension n = do 
+  let settings = Settings False True False False False
+  bench (checkSigSize dimension n) settings dimension n
+
+-- #4
+runCheckSquares :: Int -> Int -> Either String (Int, Int, Int)
+runCheckSquares dimension n = do 
+  let settings = Settings False False False True False
+  bench (checkSquares dimension n) settings dimension n
+      
+-- #5
+runCheckLength :: Int -> Int -> Either String (Int, Int, Int)
+runCheckLength dimension n = do 
+  let settings = Settings False False False True True
+  bench (checkSigLength dimension n) settings dimension n
+      
+--------------------------------------------------------------------------------
+
+-- elaborate & erase type
 elab :: (Erase ty, Num n) => Comp ty n -> Either String (TypeErased n)
 elab program = fmap eraseType (elaborate program)
 
--- elaborate & erase type & propagate constants 
+-- elaborate & erase type & propagate constants
 cp :: (Erase ty, Num n) => Comp ty n -> Either String (TypeErased n)
 cp program = ConstantPropagation.run <$> elab program
 
 comp :: (GaloisField n, Erase ty, Bounded n, Integral n) => Comp ty n -> Either String (ConstraintSystem n)
 comp program = elaborate program >>= return . compile . ConstantPropagation.run . eraseType
 
-
 optm :: (GaloisField n, Erase ty, Bounded n, Integral n) => Comp ty n -> Either String (ConstraintSystem n)
 optm program = optimise <$> comp program
 
--- partial evaluation with inputs 
-optmWithInput :: (GaloisField n, Bounded n, Integral n, Erase ty) => Comp ty n -> [n] -> Either String ([(Var ,DebugGF n)], ConstraintSystem n)
-optmWithInput program input = do 
+-- partial evaluation with inputs
+optmWithInput :: (GaloisField n, Bounded n, Integral n, Erase ty) => Comp ty n -> [n] -> Either String ([(Var, DebugGF n)], ConstraintSystem n)
+optmWithInput program input = do
   cs <- optm program
-  let (witness', cs') = optimiseWithInput input cs 
+  let (witness', cs') = optimiseWithInput input cs
   return (IntMap.toList $ fmap DebugGF witness', cs')
