1a88119987d7e3fe5a9d6844103c5342dc342c7f
Luā Tîng-Giān <banacorn@gmail.com>
2022-09-07 06:30:23 +0000

    [ change ] Benchmarking constant propogation

--- benchmark/Array.hs
+++ benchmark/Array.hs
@@ -4,13 +4,12 @@ import qualified Array.Immutable as I
 import qualified Array.Mutable as M
 import Criterion
 import Keelung (Comp, GF181, Val, elaborate)
-import Keelung.Compiler (compile)
 import qualified Keelung.Compiler as Compiler
 import qualified Keelung.Error as Language
 import Keelung.Syntax.Typed (Elaborated)
 
 benchmark :: Benchmark
-benchmark = compilation
+benchmark = fromStringOptimization1
   where
     _elaboration :: Benchmark
     _elaboration =
@@ -20,14 +19,34 @@ benchmark = compilation
           bench "Immutable" $ nf elaborate' (return $ I.fromString input)
         ]
 
-    compilation :: Benchmark
-    compilation =
+    _compilation :: Benchmark
+    _compilation =
       bgroup
         "Mutable vs Immutable Array Complilation"
-        [ bench "M.fromString" $ nf compile' (M.fromString input),
-          bench "I.fromString" $ nf compile' (return $ I.fromString input),
-          bench "M.multiply" $ nf compile' (M.multiplyT 8 2),
-          bench "I.multiply" $ nf compile' (I.multiplierT 8 2)
+        [ bench "M.fromString" $ nf compile (M.fromString input),
+          bench "I.fromString" $ nf compile (return $ I.fromString input),
+          bench "M.multiply" $ nf compile (M.multiplyT 8 2),
+          bench "I.multiply" $ nf compile (I.multiplierT 8 2)
+        ]
+
+    _fromStringCompilation :: Benchmark
+    _fromStringCompilation =
+      bgroup
+        "fromString: Complilation"
+        [ bench "I.fromString 100" $ nf compileOnly (return $ I.fromString (concat $ replicate 10 "Helloworld")),
+          bench "I.fromString 200" $ nf compileOnly (return $ I.fromString (concat $ replicate 20 "Helloworld")),
+          bench "I.fromString 400" $ nf compileOnly (return $ I.fromString (concat $ replicate 40 "Helloworld")),
+          bench "I.fromString 800" $ nf compileOnly (return $ I.fromString (concat $ replicate 80 "Helloworld"))
+        ]
+
+    fromStringOptimization1 :: Benchmark
+    fromStringOptimization1 =
+      bgroup
+        "fromString: Optimization 1"
+        [ bench "I.fromString 100" $ nf compile (return $ I.fromString (concat $ replicate 10 "Helloworld")),
+          bench "I.fromString 200" $ nf compile (return $ I.fromString (concat $ replicate 20 "Helloworld")),
+          bench "I.fromString 400" $ nf compile (return $ I.fromString (concat $ replicate 40 "Helloworld")),
+          bench "I.fromString 800" $ nf compile (return $ I.fromString (concat $ replicate 80 "Helloworld"))
         ]
 
     input :: String
@@ -36,5 +55,8 @@ benchmark = compilation
     elaborate' :: Comp (Val t) -> Either Language.ElabError Elaborated
     elaborate' = elaborate
 
-    compile' :: Comp (Val t) -> Either (Compiler.Error GF181) (Compiler.ConstraintSystem GF181)
-    compile' = compile
+    compile :: Comp (Val t) -> Either (Compiler.Error GF181) (Compiler.ConstraintSystem GF181)
+    compile = Compiler.compile
+
+    compileOnly :: Comp (Val t) -> Either (Compiler.Error GF181) (Compiler.ConstraintSystem GF181)
+    compileOnly = Compiler.compileOnly
---
+++ profiling/Main.hs
@@ -0,0 +1,67 @@
+module Main where
+
+import Array.Immutable
+import Control.Monad
+import qualified Data.Set as Set
+import Keelung (GF181, N (N))
+import Keelung.Compiler
+import qualified Keelung.Compiler.Optimize as Optimizer
+
+asGF181 :: Either (Error GF181) a -> IO a
+asGF181 (Left err) = error $ show err
+asGF181 (Right x) = return x
+
+printCS :: Bool -> ConstraintSystem GF181 -> IO ()
+printCS printConstraints cs = do
+  let constraints = csConstraints cs
+  print $ Set.size constraints
+  when printConstraints $ do
+    forM_ (Set.toList constraints) $ \constraint -> do
+      print $ fmap N constraint
+  putStrLn "========="
+
+main :: IO ()
+main = do
+  -- putStrLn "Number of constriants: fromString"
+  -- forM_ [1, 2, 4, 8] $ \i -> do
+  --   asGF181 (compile (return $ fromString (replicate i 'A'))) >>= print . Set.size . csConstraints
+
+  putStrLn "Number of constriants: fullAdder"
+  forM_ [1, 2, 4, 8] $ \i -> do
+    asGF181 (compileOnly (fullAdderT i)) >>= printCS False
+
+  forM_ [1, 2, 4, 8] $ \n -> do
+    putStrLn $ "Number of constriants: multiplier " <> show n
+    forM_ [1, 2, 4, 8] $ \i -> do
+      asGF181 (compileOnly (multiplierT n i)) >>= printCS False
+
+fa :: IO ()
+fa = do
+  putStrLn "fullAdder"
+  cs <- asGF181 (compileOnly (fullAdderT 2))
+  let constraints = csConstraints cs
+
+  putStrLn "before"
+  print $ Set.size constraints
+  forM_ (Set.toList constraints) $ \constraint -> do
+    print $ fmap N constraint
+
+  let cs' = Optimizer.optimize cs
+  let constraints' = csConstraints cs'
+
+  putStrLn "after"
+  print $ Set.size constraints'
+  forM_ (Set.toList constraints') $ \constraint -> do
+    print $ fmap N constraint
+
+  let cs'' = Optimizer.optimize2 cs'
+  let constraints'' = csConstraints cs''
+
+  putStrLn "after"
+  print $ Set.size constraints''
+  forM_ (Set.toList constraints'') $ \constraint -> do
+    print $ fmap N constraint
+
+  putStrLn "complete"
+
+  print cs''
