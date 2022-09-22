a4ef91903780407e9e1bcbd5ee97b367221dfefd
Luā Tîng-Giān <banacorn@gmail.com>
2022-09-22 06:07:07 +0000

    [ new ] Improve mutable full adder with the `reuse` directive

--- examples/Array.hs
+++ examples/Array.hs
@@ -2,6 +2,7 @@
 
 module Array where
 
+import Control.Monad
 import Keelung
 
 echo :: Comp (Val ('Arr 'Num))
@@ -9,22 +10,59 @@ echo = inputs 4
 
 modify :: Comp (Val ('Arr 'Num))
 modify = do
-    xs <- inputs 4 >>= thaw
+  xs <- inputs 4 >>= thaw
 
-    updateM xs 0 0
-    updateM xs 1 1
+  updateM xs 0 0
+  updateM xs 1 1
 
-    freeze xs
+  freeze xs
 
 init :: Comp (Val ('Arr 'Bool))
 init = do
-    let xs = toArray $ replicate 4 false
-    return xs
+  let xs = toArray $ replicate 4 false
+  return xs
 
 initM :: Comp (Val ('ArrM 'Bool))
 initM = toArrayM $ replicate 4 false
 
 initM2 :: Comp (Val ('Arr 'Bool))
 initM2 = do
-    xs <- toArrayM $ replicate 4 false
-    freeze xs
+  xs <- toArrayM $ replicate 4 false
+  freeze xs
+
+sharing :: Comp (Val ('Arr 'Num))
+sharing = do
+  x <- input
+  let y = x * x * x * x
+  return $ toArray [y, y]
+
+sharing' :: Comp (Val ('Arr 'Num))
+sharing' = do
+  x <- input
+  y <- reuse $ x * x * x * x
+  return $ toArray [y, y]
+
+fold :: Comp (Val ('Arr 'Num))
+fold = do
+  x <- input
+  (xs, _) <-
+    foldM 
+      ( \(array, acc) _ ->
+          return (array ++ [acc], acc * x)
+      )
+      ([], x)
+      ([0 .. 10] :: [Int])
+  return $ toArray xs
+
+fold' :: Comp (Val ('Arr 'Num))
+fold' = do
+  x <- input
+  (xs, _) <-
+    foldM 
+      ( \(array, acc) _ -> do 
+            acc' <- reuse $ acc * x
+            return (array ++ [acc'], acc')
+      )
+      ([], x)
+      ([0 .. 10] :: [Int])
+  return $ toArray xs
--- examples/Array/Mutable2.hs
+++ examples/Array/Mutable2.hs
@@ -2,7 +2,7 @@
 
 module Array.Mutable2 where
 
-import Control.Monad 
+import Control.Monad
 import Data.Bits (Bits (testBit))
 import Data.Word (Word8)
 import Keelung
@@ -29,21 +29,22 @@ get ref = accessM ref 0
 
 fullAdder :: Val ('ArrM 'Bool) -> Val ('ArrM 'Bool) -> Comp (Val ('ArrM 'Bool))
 fullAdder as bs = do
-  xor <- alloc
-  carryRef <- alloc
   -- allocate a new array of 64 bits for the result of the addition
   result <- toArrayM $ replicate (lengthOfM as) false
   -- 1-bit full adder
-  forM_ [0 .. lengthOfM as - 1] $ \i -> do
-    a <- accessM as i
-    b <- accessM bs i
-    set xor (a `Xor` b)
-    x <- get xor
-    carry <- get carryRef
-    let value = x `Xor` carry
-    set carryRef $ (x `And` carry) `Or` (a `And` b)
-    updateM result i value
-    return ()
+  foldM_
+    ( \carry i -> do
+        a <- accessM as i
+        b <- accessM bs i
+        x <- reuse (a `Xor` b)
+        carry' <- reuse carry
+        let value = x `Xor` carry'
+        let nextCarry = (x `And` carry') `Or` (a `And` b)
+        updateM result i value
+        return nextCarry
+    )
+    false
+    [0 .. lengthOfM as - 1]
 
   return result
 
