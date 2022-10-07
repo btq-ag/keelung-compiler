32b28f2497e27c5316d8c046616c7f42764b7ae6
Luā Tîng-Giān <banacorn@gmail.com>
2022-10-07 08:17:41 +0000

    [ refactor ] Aggregate all optimization-related tests

--- test/Main.hs
+++ test/Main.hs
@@ -6,17 +6,13 @@ import qualified AggregateSignature.Program as AggSig
 import AggregateSignature.Util
 import qualified Basic
 import Control.Arrow (ArrowChoice (right), left)
-import qualified Data.IntMap as IntMap
 import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
 import Keelung
 import Keelung.Compiler
 import qualified Keelung.Compiler as Compiler
-import Keelung.Compiler.Constraint (cadd, cmul)
+import Keelung.Compiler.Constraint (cadd)
 import Keelung.Compiler.Interpret (InterpretError (..))
-import qualified Keelung.Compiler.Optimize as Optimse
-import qualified Keelung.Compiler.Optimize.MinimizeConstraints as Optimize
-import qualified Keelung.Compiler.Optimize.Monad as Optimize
 import Keelung.Constraint.Polynomial (Poly)
 import qualified Keelung.Constraint.Polynomial as Poly
 import Keelung.Constraint.R1CS (R1CS)
@@ -42,7 +38,7 @@ main :: IO ()
 main = hspec $ do
   describe "Interpreter" Interpreter.tests
 
-  describe "Optimization" Optimization.tests 
+  describe "Optimization" Optimization.tests
 
   describe "Execution" $ do
     describe "Aggregate Signature" $ do
@@ -54,37 +50,60 @@ main = hspec $ do
         runKeelungAggSig 10 1 `shouldBe` Right []
       it "dim:10 sig:10" $
         runKeelungAggSig 10 10 `shouldBe` Right []
-
-    it "Basic.identity" $
-      execute Basic.identity [42 :: GF181] `shouldBe` Right [42]
-    it "Basic.summation" $
+    it "identity (Num)" $
+      execute Basic.identity [42] `shouldBe` Right [42 :: GF181]
+    it "identity (Bool)" $
+      execute Basic.identityB [1] `shouldBe` Right [1 :: GF181]
+    it "identity (Bool)" $
+      execute Basic.identityB [0] `shouldBe` Right [0 :: GF181]
+    it "add3" $
+      execute Basic.add3 [0] `shouldBe` Right [3 :: GF181]
+    it "eq1 1" $
+      execute Basic.eq1 [0] `shouldBe` Right [0 :: GF181]
+    it "eq1 2" $
+      execute Basic.eq1 [3] `shouldBe` Right [1 :: GF181]
+    it "cond 1" $
+      execute Basic.cond' [0] `shouldBe` Right [789 :: GF181]
+    it "assert fail" $
+      execute Basic.assert1 [0 :: GF181]
+        `shouldBe` Left
+          ( InterpretError $
+              InterpretAssertionError
+                (C.Eq (C.Var (C.NumInputVar 0)) (C.Val (C.Integer 3)))
+                mempty
+                [0]
+          )
+    it "assert success" $
+      execute Basic.assert1 [3] `shouldBe` Right [3 :: GF181]
+    it "toArrayM" $
+      execute Basic.toArrayM1 [] `shouldBe` Right ([0] :: [GF181])
+    it "summation" $
       execute Basic.summation [0, 2, 4, 8 :: GF181] `shouldBe` Right [14]
-    it "Basic.summation2" $
+    it "summation2" $
       execute Basic.summation2 [0, 2, 4, 8 :: GF181] `shouldBe` Right []
-    it "Basic.assertArraysEqual" $
+    it "assertArraysEqual" $
       execute Basic.assertArraysEqual [0, 2, 4, 8, 0, 2, 4, 8 :: GF181] `shouldBe` Right []
-    it "Basic.assertArraysEqual2" $
+    it "assertArraysEqual2" $
       execute Basic.assertArraysEqual2 [0, 2, 4, 8, 0, 2, 4, 8 :: GF181] `shouldBe` Right []
-
-    it "Basic.array1D" $
+    it "array1D" $
       execute (Basic.array1D 1) [2, 4 :: GF181] `shouldBe` Right []
 
-    it "Basic.array2D 1" $
+    it "array2D 1" $
       execute (Basic.array2D 1 1) [2, 4 :: GF181] `shouldBe` Right []
 
-    it "Basic.array2D 2" $
+    it "array2D 2" $
       execute (Basic.array2D 2 2) [0, 1, 2, 3, 0, 1, 4, 9 :: GF181] `shouldBe` Right []
 
-    it "Basic.toArray1" $
+    it "toArray1" $
       execute Basic.toArray1 [0 .. 7 :: GF181] `shouldBe` Right []
 
-    it "Basic.xorLists" $
+    it "xorLists" $
       execute Basic.xorLists [] `shouldBe` Right [1 :: GF181]
 
-    it "Basic.dupArray" $
+    it "dupArray" $
       execute Basic.dupArray [1] `shouldBe` Right [1 :: GF181]
 
-    it "Basic.returnArray2" $
+    it "returnArray2" $
       execute Basic.returnArray2 [2] `shouldBe` Right [2, 4 :: GF181]
 
   describe "Type Erasure" $ do
@@ -127,179 +146,7 @@ main = hspec $ do
               }
        in Compiler.compile Basic.assertToBe42 `shouldBe` Right cs
 
-  describe "Compilation" $ do
-    it "identity (Num)" $
-      execute Basic.identity [42] `shouldBe` Right [42 :: GF181]
-    it "identity (Bool)" $
-      execute Basic.identityB [1] `shouldBe` Right [1 :: GF181]
-    it "identity (Bool)" $
-      execute Basic.identityB [0] `shouldBe` Right [0 :: GF181]
-    it "add3" $
-      execute Basic.add3 [0] `shouldBe` Right [3 :: GF181]
-    it "eq1 1" $
-      execute Basic.eq1 [0] `shouldBe` Right [0 :: GF181]
-    it "eq1 2" $
-      execute Basic.eq1 [3] `shouldBe` Right [1 :: GF181]
-    it "cond 1" $
-      execute Basic.cond' [0] `shouldBe` Right [789 :: GF181]
-    it "assert fail" $
-      execute Basic.assert1 [0 :: GF181]
-        `shouldBe` Left
-          ( InterpretError $
-              InterpretAssertionError
-                (C.Eq (C.Var (C.NumInputVar 0)) (C.Val (C.Integer 3)))
-                mempty
-                [0]
-          )
-    it "assert success" $
-      execute Basic.assert1 [3] `shouldBe` Right [3 :: GF181]
-
-    it "toArrayM" $
-      execute Basic.toArrayM1 [] `shouldBe` Right ([0] :: [GF181])
-
-  -- NOTE:
-  --    some variables are of "don't care"
-  --    they get thrown away and won't be in the witness
-  -- it "cond 2" $
-  --   execute Basic.cond [3] `shouldBe` Right 12
-
-  describe "Optimisation" $ do
-    -- describe "Constant Propagation" $ do
-    --   it "1 + 1" $
-    --     erasedExpr <$> Basic.elab Basic.constant1 `shouldBe` Right 2
-
-    describe "Constraint Set Reduction" $ do
-      it "$0 = $1" $
-        let constraint = head $ cadd 0 [(0, 1), (1, -1)]
-            links = IntMap.fromList [(1, 0)]
-            sizes = IntMap.fromList [(0, 2)]
-            go :: Optimize.OptiM GF181 a -> a
-            go = Optimize.runOptiM' links sizes mempty
-         in go (Optimize.substConstraint constraint)
-              `shouldBe` Nothing
-
-      it "should work 1" $
-        let cs =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      concat
-                        [ cadd 0 [(0, 4972), (1, 10582), (16, -1)],
-                          cadd 0 [(0, 10582), (1, 7317), (17, -1)],
-                          cadd 0 [(2, 3853), (3, 4216), (15, -1)],
-                          cadd 0 [(2, 8073), (3, 3853), (14, -1)],
-                          cadd 0 [(4, 1), (8, 12289), (17, -1)],
-                          cadd 0 [(5, 1), (9, 12289), (16, -1)],
-                          cadd 0 [(6, 1), (10, 12289), (15, -1)],
-                          cadd 0 [(7, 1), (11, 12289), (14, -1)],
-                          cadd 0 [(4, 1), (6, 1), (13, -1)],
-                          cadd 0 [(5, 1), (7, 1), (12, -1)],
-                          cadd 10623 [(13, -1)],
-                          cadd 11179 [(12, -1)]
-                        ],
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 17],
-                  csInputVarSize = 12,
-                  csOutputVarSize = 0
-                }
-         in Optimse.optimize1 (cs :: ConstraintSystem GF181) `shouldNotBe` cs
-
-    describe "Constraint merging" $ do
-      it "CAdd & CAdd" $
-        let cs =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cadd 0 [(0, 1), (1, 1), (4, 1)]
-                        ++ cadd 0 [(2, 1), (3, 1), (4, 1)],
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 4],
-                  csInputVarSize = 4,
-                  csOutputVarSize = 0
-                }
-            cs' =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cadd 0 [(0, 1), (1, 1), (2, -1), (3, -1)],
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 3],
-                  csInputVarSize = 4,
-                  csOutputVarSize = 0
-                }
-         in Optimse.optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
-
-      it "CAdd & CMul 1" $
-        let cs =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cmul [(3, 1)] [(2, 1)] (42, []) --- $3 * $2 = 42
-                        ++ cadd 0 [(3, 1), (0, 1), (1, 1)], --- 0 = $3 + $0 + $1
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 3],
-                  csInputVarSize = 3,
-                  csOutputVarSize = 0
-                }
-            cs' =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList (cmul [(0, -1), (1, -1)] [(2, 1)] (42, [])), -- (- $0 - $1) * $2 = 42
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 2],
-                  csInputVarSize = 3,
-                  csOutputVarSize = 0
-                }
-         in Optimse.optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
-
-      it "CAdd & CMul 2" $
-        let cs =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cadd 0 [(3, 1), (0, 1), (1, 1)] --- 0 = $3 + $0 + $1
-                        ++ cmul [(2, 1)] [(3, 1)] (42, []), --- $2 * $3 = 42
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 3],
-                  csInputVarSize = 3,
-                  csOutputVarSize = 0
-                }
-            cs' =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList (cmul [(0, -1), (1, -1)] [(2, 1)] (42, [])), -- (- $0 - $1) * $2 = 42
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 2],
-                  csInputVarSize = 3,
-                  csOutputVarSize = 0
-                }
-         in Optimse.optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
-
-      it "CAdd & CMul 3" $
-        let cs =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cadd 0 [(4, 1), (0, 1), (1, 1)] --- 0 = $4 + $0 + $1
-                        ++ cmul [(2, 1)] [(3, 1)] (0, [(4, 1)]), --- $2 * $3 = $4
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 4],
-                  csInputVarSize = 4,
-                  csOutputVarSize = 0
-                }
-            cs' =
-              ConstraintSystem
-                { csConstraints =
-                    Set.fromList $
-                      cmul [(2, 1)] [(3, 1)] (0, [(0, -1), (1, -1)]), --- $2 * $3 = - $0 - $1
-                  csBoolVars = mempty,
-                  csVars = IntSet.fromList [0 .. 3],
-                  csInputVarSize = 4,
-                  csOutputVarSize = 0
-                }
-         in Optimse.optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
-
-  describe "Compile" $ do
+  describe "Keelung `compile`" $ do
     it "Program that throws ElabError.IndexOutOfBoundsError" $ do
       let expected = left show ((toR1CS :: ConstraintSystem GF181 -> R1CS GF181) <$> Compiler.optimize1 Basic.outOfBound)
       actual <- right (fmap fromInteger) . left show <$> Keelung.compile GF181 Basic.outOfBound
@@ -315,7 +162,7 @@ main = hspec $ do
       actual <- right (fmap fromInteger) . left show <$> Keelung.compile GF181 Basic.identity
       actual `shouldBe` expected
 
-  describe "Interpret" $ do
+  describe "Keelung `interpret`" $ do
     it "Program that throws ElabError.IndexOutOfBoundsError" $ do
       let expected = left show (Compiler.interpret Basic.outOfBound ([] :: [GF181]))
       actual <- left show <$> Keelung.interpret_ GF181 Basic.outOfBound []
--- test/Test/Optimization.hs
+++ test/Test/Optimization.hs
@@ -1,15 +1,154 @@
 module Test.Optimization (tests) where
 
 import qualified Basic
+import qualified Data.IntMap as IntMap
+import qualified Data.IntSet as IntSet
+import qualified Data.Set as Set
 import Keelung (Comp, Elaborable, GF181)
-import Keelung.Compiler (asGF181, optimize1, toR1CS)
+import Keelung.Compiler (asGF181, toR1CS)
+import qualified Keelung.Compiler as Compiler
+import Keelung.Compiler.Constraint
 import Keelung.Compiler.Error (Error)
+import Keelung.Compiler.Optimize
+import qualified Keelung.Compiler.Optimize.MinimizeConstraints as O1
+import Keelung.Compiler.Optimize.Monad
 import Keelung.Constraint.R1CS (toR1Cs)
 import Test.Hspec
 
 tests :: SpecWith ()
 tests = do
-  describe "Constraint Number" $ do
+  describe "Constraint set reduction (O1)" $ do
+    it "$0 = $1" $
+      let constraint = head $ cadd 0 [(0, 1), (1, -1)]
+          links = IntMap.fromList [(1, 0)]
+          sizes = IntMap.fromList [(0, 2)]
+          go :: OptiM GF181 a -> a
+          go = runOptiM' links sizes mempty
+       in go (O1.substConstraint constraint)
+            `shouldBe` Nothing
+
+    it "should work 1" $
+      let cs =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    concat
+                      [ cadd 0 [(0, 4972), (1, 10582), (16, -1)],
+                        cadd 0 [(0, 10582), (1, 7317), (17, -1)],
+                        cadd 0 [(2, 3853), (3, 4216), (15, -1)],
+                        cadd 0 [(2, 8073), (3, 3853), (14, -1)],
+                        cadd 0 [(4, 1), (8, 12289), (17, -1)],
+                        cadd 0 [(5, 1), (9, 12289), (16, -1)],
+                        cadd 0 [(6, 1), (10, 12289), (15, -1)],
+                        cadd 0 [(7, 1), (11, 12289), (14, -1)],
+                        cadd 0 [(4, 1), (6, 1), (13, -1)],
+                        cadd 0 [(5, 1), (7, 1), (12, -1)],
+                        cadd 10623 [(13, -1)],
+                        cadd 11179 [(12, -1)]
+                      ],
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 17],
+                csInputVarSize = 12,
+                csOutputVarSize = 0
+              }
+       in optimize1 (cs :: ConstraintSystem GF181) `shouldNotBe` cs
+
+  describe "Constraint merging (O2)" $ do
+    it "CAdd & CAdd" $
+      let cs =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cadd 0 [(0, 1), (1, 1), (4, 1)]
+                      ++ cadd 0 [(2, 1), (3, 1), (4, 1)],
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 4],
+                csInputVarSize = 4,
+                csOutputVarSize = 0
+              }
+          cs' =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cadd 0 [(0, 1), (1, 1), (2, -1), (3, -1)],
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 3],
+                csInputVarSize = 4,
+                csOutputVarSize = 0
+              }
+       in optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
+
+    it "CAdd & CMul 1" $
+      let cs =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cmul [(3, 1)] [(2, 1)] (42, []) --- $3 * $2 = 42
+                      ++ cadd 0 [(3, 1), (0, 1), (1, 1)], --- 0 = $3 + $0 + $1
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 3],
+                csInputVarSize = 3,
+                csOutputVarSize = 0
+              }
+          cs' =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList (cmul [(0, -1), (1, -1)] [(2, 1)] (42, [])), -- (- $0 - $1) * $2 = 42
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 2],
+                csInputVarSize = 3,
+                csOutputVarSize = 0
+              }
+       in optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
+
+    it "CAdd & CMul 2" $
+      let cs =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cadd 0 [(3, 1), (0, 1), (1, 1)] --- 0 = $3 + $0 + $1
+                      ++ cmul [(2, 1)] [(3, 1)] (42, []), --- $2 * $3 = 42
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 3],
+                csInputVarSize = 3,
+                csOutputVarSize = 0
+              }
+          cs' =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList (cmul [(0, -1), (1, -1)] [(2, 1)] (42, [])), -- (- $0 - $1) * $2 = 42
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 2],
+                csInputVarSize = 3,
+                csOutputVarSize = 0
+              }
+       in optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
+
+    it "CAdd & CMul 3" $
+      let cs =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cadd 0 [(4, 1), (0, 1), (1, 1)] --- 0 = $4 + $0 + $1
+                      ++ cmul [(2, 1)] [(3, 1)] (0, [(4, 1)]), --- $2 * $3 = $4
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 4],
+                csInputVarSize = 4,
+                csOutputVarSize = 0
+              }
+          cs' =
+            ConstraintSystem
+              { csConstraints =
+                  Set.fromList $
+                    cmul [(2, 1)] [(3, 1)] (0, [(0, -1), (1, -1)]), --- $2 * $3 = - $0 - $1
+                csBoolVars = mempty,
+                csVars = IntSet.fromList [0 .. 3],
+                csInputVarSize = 4,
+                csOutputVarSize = 0
+              }
+       in optimize2 (cs :: ConstraintSystem GF181) `shouldBe` cs'
+
+  describe "Constraint number counting" $ do
     describe "AND Chaining" $ do
       it "1 variable" $ count (Basic.chainingAND 1) `shouldBe` Right (1 + 1)
       it "2 variables" $ count (Basic.chainingAND 2) `shouldBe` Right (2 + 1)
@@ -26,8 +165,7 @@ tests = do
       it "6 variables" $ count (Basic.chainingOR 6) `shouldBe` Right (6 + 5)
       it "7 variables" $ count (Basic.chainingOR 7) `shouldBe` Right (7 + 5)
   where
-    -- | Count the number of generated constraints from a given program.
     count :: Elaborable t => Comp t -> Either (Error GF181) Int
     count program = do
-      cs <- asGF181 (optimize1 program)
+      cs <- asGF181 (Compiler.optimize1 program)
       return $ length $ toR1Cs $ toR1CS cs
