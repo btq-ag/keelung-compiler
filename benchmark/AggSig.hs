2252cff18e037891d39bba382bc8d8f86f2068d0
Luā Tîng-Giān <banacorn@gmail.com>
2022-10-12 06:41:49 +0000

    [ refactor ] Remove Benchmark.Util

--- benchmark/AggSig.hs
+++ benchmark/AggSig.hs
@@ -4,9 +4,9 @@ module AggSig (run) where
 
 import qualified AggregateSignature.Program as AggSig
 import AggregateSignature.Util
-import Benchmark.Util
 import Criterion
-import Keelung (GF181, Comp)
+import Keelung (Comp, GF181, elaborate)
+import Keelung.Compiler
 
 run :: Benchmark
 run =
@@ -19,17 +19,17 @@ run =
     ]
   where
     program :: Int -> Comp ()
-    program n = 
-        let dimension = 128
-            numberOfSignatures = n 
-            settings =
-              Settings
-                { enableAggChecking = True,
-                  enableSizeChecking = True,
-                  enableLengthChecking = True
-                }
-            setup = makeParam dimension numberOfSignatures 42 settings :: Param GF181
-        in AggSig.aggregateSignature setup
+    program n =
+      let dimension = 128
+          numberOfSignatures = n
+          settings =
+            Settings
+              { enableAggChecking = True,
+                enableSizeChecking = True,
+                enableLengthChecking = True
+              }
+          setup = makeParam dimension numberOfSignatures 42 settings :: Param GF181
+       in AggSig.aggregateSignature setup
 
     elaboration :: Benchmark
     elaboration =
@@ -41,16 +41,16 @@ run =
     compilation =
       bgroup
         "Compilation"
-        $ map (\i -> bench (show i) $ nf compile $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileOnly) $ program i) [1, 2, 4, 8]
 
     constantPropagation :: Benchmark
     constantPropagation =
       bgroup
         "Constant Propagation"
-        $ map (\i -> bench (show i) $ nf optimize0 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileO0) $ program i) [1, 2, 4, 8]
 
     optimization1 :: Benchmark
     optimization1 =
       bgroup
         "Optimization I"
-        $ map (\i -> bench (show i) $ nf optimize1 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileO1) $ program i) [1, 2, 4, 8]
--- benchmark/Array.hs
+++ benchmark/Array.hs
@@ -4,8 +4,8 @@ module Array (fromString, fullAdder, multiplier) where
 
 import qualified Array.Immutable as I
 import Criterion
-import Benchmark.Util
-import Keelung (Comp, Arr, Boolean)
+import Keelung (Arr, Boolean, Comp, elaborate)
+import Keelung.Compiler
 
 fromString :: Benchmark
 fromString =
@@ -30,19 +30,19 @@ fromString =
     compilation =
       bgroup
         "Compilation"
-        $ map (\i -> bench (show (i * 1000)) $ nf compile $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 1000)) $ nf (asGF181 . compileOnly) $ program i) [1, 2, 4, 8]
 
     constantPropagation :: Benchmark
     constantPropagation =
       bgroup
         "Constant Propagation"
-        $ map (\i -> bench (show (i * 1000)) $ nf optimize0 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 1000)) $ nf (asGF181 . compileO0) $ program i) [1, 2, 4, 8]
 
     optimization1 :: Benchmark
     optimization1 =
       bgroup
         "Optimization I"
-        $ map (\i -> bench (show (i * 1000)) $ nf optimize1 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 1000)) $ nf (asGF181 . compileO1) $ program i) [1, 2, 4, 8]
 
 fullAdder :: Benchmark
 fullAdder =
@@ -67,19 +67,19 @@ fullAdder =
     compilation =
       bgroup
         "Compilation"
-        $ map (\i -> bench (show (i * 10)) $ nf compile $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 10)) $ nf (asGF181 . compileOnly) $ program i) [1, 2, 4, 8]
 
     constantPropagation :: Benchmark
     constantPropagation =
       bgroup
         "Constant Propagation"
-        $ map (\i -> bench (show (i * 10)) $ nf optimize0 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 10)) $ nf (asGF181 . compileO0) $ program i) [1, 2, 4, 8]
 
     optimization1 :: Benchmark
     optimization1 =
       bgroup
         "Optimization I"
-        $ map (\i -> bench (show (i * 10)) $ nf optimize1 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show (i * 10)) $ nf (asGF181 . compileO1) $ program i) [1, 2, 4, 8]
 
 multiplier :: Benchmark
 multiplier =
@@ -104,19 +104,19 @@ multiplier =
     compilation =
       bgroup
         "Compilation"
-        $ map (\i -> bench (show i) $ nf compile $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileOnly) $ program i) [1, 2, 4, 8]
 
     constantPropagation :: Benchmark
     constantPropagation =
       bgroup
         "Constant Propagation"
-        $ map (\i -> bench (show i) $ nf optimize0 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileO0) $ program i) [1, 2, 4, 8]
 
     optimization1 :: Benchmark
     optimization1 =
       bgroup
         "Optimization I"
-        $ map (\i -> bench (show i) $ nf optimize1 $ program i) [1, 2, 4, 8]
+        $ map (\i -> bench (show i) $ nf (asGF181 . compileO1) $ program i) [1, 2, 4, 8]
 
 -- optimization3 :: Benchmark
 -- optimization3 =
