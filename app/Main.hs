2d3edddf151c4048b1fd1cce33d81cbed3a6a535
Ting-gian LUA <banacorn@gmail.com>
2023-02-15 22:49:07 +0800

    [ dep ] Bump version to v0.8.3

--- app/Main.hs
+++ app/Main.hs
@@ -97,7 +97,7 @@ main = withUtf8 $ do
             BN128 ->
               outputInterpretedResultAndWriteFile
                 (genInputsOutputsWitnessesElab elaborated (map fromInteger inputs :: [BN128]))
-    Version -> putStrLn "Keelung v0.8.2"
+    Version -> putStrLn "Keelung v0.8.3"
   where
     asB64 :: Either (Error B64) (RelocatedConstraintSystem B64) -> Either (Error B64) (RelocatedConstraintSystem B64)
     asB64 = id
