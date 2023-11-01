6ecce11cb5da7e416eaee5c796957cf2ec0c30fb
Ting-gian LUA <banacorn@gmail.com>
2023-11-01 10:13:27 +0800

    [ refactor ] Replace `chooseInt (0, 255)` with just `arbitrary :: Gen Word8` in randomized property-based testings

--- test/Test/Compilation/UInt/AESMul.hs
+++ test/Test/Compilation/UInt/AESMul.hs
@@ -6,6 +6,7 @@ module Test.Compilation.UInt.AESMul (tests, run) where
 
 -- import Data.Word
 
+import Control.Monad
 import Data.Word (Word8)
 import Keelung hiding (compile)
 import Keelung.Data.U qualified as U
@@ -24,28 +25,19 @@ tests =
     it "Constants only" $ do
       let program x y = do
             return $ x `aesMul` (y :: UInt 8)
-      let genPair = do
-            x <- choose (0, 255)
-            y <- choose (0, 255)
-            return (x, y)
-      forAll genPair $ \(x, y) -> do
-        let expected = [U.uValue (U.aesMul (U.new 8 x) (U.new 8 y))]
-        runAll (Prime 5) (program (fromInteger x) (fromInteger y)) [] [] expected
-        runAll (Prime 257) (program (fromInteger x) (fromInteger y)) [] [] expected
+      property $ \(x :: Word8, y :: Word8) -> do
+        let expected = [U.uValue (U.aesMul (U.new 8 (toInteger x)) (U.new 8 (toInteger y)))]
+        forM_ [gf181, Prime 5] $ \field -> do
+          runAll field (program (fromIntegral x) (fromIntegral y)) [] [] expected
 
     it "1 variable / 1 constant" $ do
       let program constant = do
             x <- inputUInt @8 Public
             return $ x `aesMul` fromInteger constant
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            constant <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger constant)
-      forAll genPair $ \(x, constant) -> do
+      property $ \(x :: Word8, constant :: Word8) -> do
         let expected = [U.uValue (U.aesMul (U.new 8 (toInteger x)) (U.new 8 (toInteger constant)))]
-        runAll (Prime 17) (program constant) [x] [] expected
-        runAll (Prime 257) (program constant) [x] [] expected
-        runAll (Prime 1031) (program constant) [x] [] expected
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral constant)) (map toInteger [x]) [] expected
 
     it "2 byte variables" $ do
       let program = do
@@ -53,26 +45,19 @@ tests =
             y <- inputUInt @8 Public
             return $ x `aesMul` y
       property $ \(x :: Word8, y :: Word8) -> do
-        let expected = [U.uValue (U.aesMul (U.new 8 (toInteger x)) (U.new 8 (toInteger y)))]
-        runAll (Prime 17) program [toInteger x, toInteger y] [] expected
-        runAll (Prime 257) program [toInteger x, toInteger y] [] expected
-        runAll (Prime 1031) program [toInteger x, toInteger y] [] expected
+        let expected = map toInteger [U.aesMul (U.new 8 (toInteger x)) (U.new 8 (toInteger y))]
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field program (map toInteger [x, y]) [] expected
 
     it "2 variables / 1 constant" $ do
       let program c = do
             x <- inputUInt @8 Public
             y <- inputUInt @8 Public
             return $ x .*. y .*. fromInteger c
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            y <- (arbitrary :: Gen Word8)
-            c <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger y, toInteger c)
-      forAll genPair $ \(x, y, c) -> do
-        let expected = [U.uValue (U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger c))]
-        runAll (Prime 17) (program c) [x, y] [] expected
-        runAll (Prime 257) (program c) [x, y] [] expected
-        runAll (Prime 1031) (program c) [x, y] [] expected
+      property $ \(x, y :: Word8, c :: Word8) -> do
+        let expected = map toInteger [U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger c)]
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral c)) (map toInteger [x, y]) [] expected
 
     it "3 variables / 1 constant" $ do
       let program c = do
@@ -80,14 +65,7 @@ tests =
             y <- inputUInt @8 Public
             z <- inputUInt @8 Public
             return $ x .*. y .*. z .*. fromInteger c
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            y <- (arbitrary :: Gen Word8)
-            z <- (arbitrary :: Gen Word8)
-            c <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger y, toInteger z, toInteger c)
-      forAll genPair $ \(x, y, z, c) -> do
-        let expected = [U.uValue (U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger z) `U.clMul` U.new 8 (toInteger c))]
-        runAll (Prime 17) (program c) [x, y, z] [] expected
-        runAll (Prime 257) (program c) [x, y, z] [] expected
-        runAll (Prime 1031) (program c) [x, y, z] [] expected
+      property $ \(x, y, z :: Word8, c :: Word8) -> do
+        let expected = map toInteger [U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger z) `U.clMul` U.new 8 (toInteger c)]
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral c)) (map toInteger [x, y, z]) [] expected
--- test/Test/Compilation/UInt/CLMul.hs
+++ test/Test/Compilation/UInt/CLMul.hs
@@ -4,11 +4,12 @@
 
 module Test.Compilation.UInt.CLMul (tests, run) where
 
+import Control.Monad
 import Data.Word
 import Keelung hiding (compile)
 import Keelung.Data.U qualified as U
-import Test.Hspec
 import Test.Compilation.Util
+import Test.Hspec
 import Test.QuickCheck hiding ((.&.))
 
 run :: IO ()
@@ -36,52 +37,29 @@ tests =
             x <- inputUInt @8 Public
             y <- inputUInt @8 Public
             return $ x .*. y
-      property $ \(x :: Word8, y :: Word8) -> do
+      property $ \(x, y :: Word8) -> do
         let expected = [U.uValue (U.clMul (U.new 8 (toInteger x)) (U.new 8 (toInteger y)))]
-        runAll (Prime 17) program [toInteger x, toInteger y] [] expected
-        runAll (Prime 257) program [toInteger x, toInteger y] [] expected
-        runAll (Prime 1031) program [toInteger x, toInteger y] [] expected
-
-    --     it "2 Word64 variables" $ do
-    --       let program = do
-    --             x <- inputUInt @64 Public
-    --             y <- inputUInt @64 Public
-    --             return $ x .*. y
-    --       property $ \(x :: Word64, y :: Word64) -> do
-    --         let expected = [U.uValue (U.clMul (U.new 64 (toInteger x)) (U.new 64 (toInteger y)))]
-    --         --   runAll (Prime 17) program [toInteger x, toInteger y] [] expected
-    --         --   runAll (Prime 1031) program [toInteger x, toInteger y] [] expected
-    --         runAll gf181 program [toInteger x, toInteger y] [] expected
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field program (map toInteger [x, y]) [] expected
 
     it "1 variable / 1 constant" $ do
       let program y = do
             x <- inputUInt @8 Public
             return $ x .*. fromInteger y
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            y <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger y)
-      forAll genPair $ \(x, y) -> do
+      property $ \(x :: Word8, y :: Word8) -> do
         let expected = [U.uValue (U.clMul (U.new 8 (toInteger x)) (U.new 8 (toInteger y)))]
-        runAll (Prime 17) (program y) [x] [] expected
-        runAll (Prime 257) (program y) [x] [] expected
-        runAll (Prime 1031) (program y) [x] [] expected
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral y)) [toInteger x] [] expected
 
     it "2 variables / 1 constant" $ do
       let program c = do
             x <- inputUInt @8 Public
             y <- inputUInt @8 Public
             return $ x .*. y .*. fromInteger c
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            y <- (arbitrary :: Gen Word8)
-            c <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger y, toInteger c)
-      forAll genPair $ \(x, y, c) -> do
+      property $ \(x, y :: Word8, c :: Word8) -> do
         let expected = [U.uValue (U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger c))]
-        runAll (Prime 17) (program c) [x, y] [] expected
-        runAll (Prime 257) (program c) [x, y] [] expected
-        runAll (Prime 1031) (program c) [x, y] [] expected
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral c)) (map toInteger [x, y]) [] expected
 
     it "3 variables / 1 constant" $ do
       let program c = do
@@ -89,14 +67,7 @@ tests =
             y <- inputUInt @8 Public
             z <- inputUInt @8 Public
             return $ x .*. y .*. z .*. fromInteger c
-      let genPair = do
-            x <- (arbitrary :: Gen Word8)
-            y <- (arbitrary :: Gen Word8)
-            z <- (arbitrary :: Gen Word8)
-            c <- (arbitrary :: Gen Word8)
-            return (toInteger x, toInteger y, toInteger z, toInteger c)
-      forAll genPair $ \(x, y, z, c) -> do
+      property $ \(x, y, z :: Word8, c :: Word8) -> do
         let expected = [U.uValue (U.new 8 (toInteger x) `U.clMul` U.new 8 (toInteger y) `U.clMul` U.new 8 (toInteger z) `U.clMul` U.new 8 (toInteger c))]
-        runAll (Prime 17) (program c) [x, y, z] [] expected
-        runAll (Prime 257) (program c) [x, y, z] [] expected
-        runAll (Prime 1031) (program c) [x, y, z] [] expected
+        forM_ [gf181, Prime 17] $ \field -> do
+          runAll field (program (fromIntegral c)) (map toInteger [x, y, z]) [] expected
