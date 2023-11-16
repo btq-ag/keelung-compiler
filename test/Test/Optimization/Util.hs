6586a7cbcd7c49eef78b2ccbce35c484bcd337a4
Ting-gian LUA <banacorn@gmail.com>
2023-11-16 18:19:27 +0800

    [ new ] Utils for testing constraint size on binary fields

--- test/Test/Optimization/Util.hs
+++ test/Test/Optimization/Util.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Test.Optimization.Util (debug, executeGF181, executePrime, shouldHaveSize) where
+module Test.Optimization.Util (debug, executeGF181, executePrime, executeBinary, shouldHaveSize) where
 
 import Control.Arrow (left)
 import Control.Monad ((>=>))
-import Data.Field.Galois (Prime)
+import Data.Field.Galois (Prime, Binary)
 import Data.Proxy (Proxy)
 import Keelung hiding (compile)
 import Keelung.Compiler (Error)
@@ -49,32 +49,20 @@ executePrime n program = caseFieldType (Prime n) handlePrime undefined
           cmCounters cm `shouldBe` cmCounters cm'
           return (cm, cm')
 
--- execute :: Encode t => FieldType -> Comp t -> IO (ConstraintModule Integer, ConstraintModule Integer)
--- execute fieldType program = caseFieldType fieldType handlePrime handleBinary
---   where
---     handlePrime :: KnownNat n => Proxy (Prime n) -> FieldInfo -> IO (ConstraintModule Integer, ConstraintModule Integer)
---     handlePrime (_ :: Proxy (Prime n)) fieldInfo = do
---       cm <- case compile (fieldInfo :: FieldInfo) program of
---         Left err -> assertFailure $ show err
---         Right result -> return (fmap toInteger result)
---       case optimize cm of
---         Left err -> assertFailure $ show err
---         Right cm' -> do
---           -- var counters should remain the same
---           cmCounters cm `shouldBe` cmCounters cm'
---           return (cm, cm')
-
---     handleBinary :: KnownNat n => Proxy (Binary n) -> FieldInfo -> IO (ConstraintModule Integer, ConstraintModule Integer)
---     handleBinary (_ :: Proxy (Binary n)) fieldInfo = do
---       cm <- case compile (fieldInfo :: FieldInfo) program of
---         Left err -> assertFailure $ show err
---         Right result -> return result
---       case optimize cm of
---         Left err -> assertFailure $ show err
---         Right cm' -> do
---           -- var counters should remain the same
---           cmCounters cm `shouldBe` cmCounters cm'
---           return (cm, cm')
+-- | Returns the original and optimized constraint system
+executeBinary :: (Encode t, GaloisField n, Integral n) => Integer -> Comp t -> IO (ConstraintModule n, ConstraintModule n)
+executeBinary n program = caseFieldType (Binary n) undefined handleBinary
+  where
+    handleBinary (_ :: Proxy (Binary n)) fieldInfo = do
+      cm <- case compile (fieldInfo :: FieldInfo) program of
+        Left err -> assertFailure $ show err
+        Right result -> return result
+      case optimize cm of
+        Left err -> assertFailure $ show err
+        Right cm' -> do
+          -- var counters should remain the same
+          cmCounters cm `shouldBe` cmCounters cm'
+          return (cm, cm')
 
 shouldHaveSize :: ConstraintModule (N GF181) -> Int -> IO ()
 shouldHaveSize cm expectedBeforeSize = do
