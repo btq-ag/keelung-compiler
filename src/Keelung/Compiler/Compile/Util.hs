7d99d1f86b4f24d46a49d591c8a37a7a92f05d7b
Ting-gian LUA <banacorn@gmail.com>
2024-04-15 11:28:42 +0800

    [ change ] Optimize `Compiler.Util.calculateSignsOfLimbs` a bit

--- src/Keelung/Compiler/Compile/Util.hs
+++ src/Keelung/Compiler/Compile/Util.hs
@@ -35,23 +35,27 @@ calculateBounds constant = foldl step (constant, constant)
 calculateCarrySigns :: Int -> Integer -> Seq Limb -> [Bool]
 calculateCarrySigns limbWidth constant limbs = drop limbWidth $ calculateSignsOfLimbs limbWidth constant limbs
 
+-- | TODO: optimize this function (along with `calculateBounds` and `widthOfInteger` ...)
 -- | Calculate the signs of bits of the summation of some Limbs and a constant
 calculateSignsOfLimbs :: Int -> Integer -> Seq Limb -> [Bool]
 calculateSignsOfLimbs limbWidth constant limbs =
   let (lower_, upper) = calculateBounds constant limbs
-      -- if the lower bound is negative, round it to the nearest multiple of `2 ^ limbWidth` smaller than it!
-      lower = if lower_ < 0 then (lower_ `div` (2 ^ limbWidth)) * 2 ^ limbWidth else lower_
+   in if lower_ >= 0
+        then
+          let width = widthOfInteger upper
+           in replicate (width `max` limbWidth) True
+        else -- if the lower bound is negative, round it to the nearest multiple of `2 ^ limbWidth` smaller than it!
 
-      signs = rangeToBitSigns (lower, upper)
-      numberOfSigns = length signs
-   in -- pad the signs to the width of limbs if necessary
-      signs <> replicate (limbWidth - numberOfSigns) True
+          let lower = (lower_ `div` (2 ^ limbWidth)) * 2 ^ limbWidth
+              width = widthOfInteger (upper - lower)
+           in -- pad the signs to the width of limbs if necessary
+              map (not . Data.Bits.testBit (-lower)) [0 .. width - 1] <> replicate (limbWidth - width) True
 
 -- | Given a range, calculate the signs of bits such that the range can be represented by the bits
 rangeToBitSigns :: (Integer, Integer) -> [Bool]
 rangeToBitSigns (lower, upper) =
   let range = upper - lower
-      width = widthOfInteger (fromInteger (-lower) `max` fromInteger upper `max` range)
+      width = widthOfInteger ((-lower) `max` upper `max` range)
    in if lower >= 0
         then replicate width True
         else map (not . Data.Bits.testBit (-lower)) [0 .. width - 1]
@@ -65,19 +69,3 @@ bitSignsToRange =
           if sign then (index + 1, (lower, higher + 2 ^ index)) else (index + 1, (lower - 2 ^ index, higher))
       )
       (0 :: Int, (0, 0))
-
---  in if lower < 0
---       then
---         -- if the lower bound is still >= `-2 ^ limbWidth`, we max it to `-2 ^ limbWidth` for the ease of calculation
---         if (-lower) <= 2 ^ limbWidth
---           then
---             let range = upper + (2 ^ limbWidth)
---                 carryWidth = widthOfInteger range
---              in False : replicate (carryWidth - limbWidth - 1) True
---           else
---             let range = upper - lower -- + 2 ^ limbWidth - 1
---                 carryWidth = widthOfInteger range
---              in map (not . Data.Bits.testBit (-lower + 2 ^ limbWidth)) [limbWidth .. carryWidth - 1]
---       else
---         let carryWidth = widthOfInteger upper
---          in replicate (carryWidth - limbWidth) True
