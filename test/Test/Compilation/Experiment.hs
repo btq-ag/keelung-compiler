5cceb0f651996019caa30d125dbaf993faf76d6e
Ting-gian LUA <banacorn@gmail.com>
2024-04-22 16:45:08 +0800

    [ fix ] UInt multiplication

--- test/Test/Compilation/Experiment.hs
+++ test/Test/Compilation/Experiment.hs
@@ -22,12 +22,6 @@ run = hspec tests
 
 tests :: SpecWith ()
 tests = describe "Experiment" $ do
-  -- it "failed" $ do
-  --   let program = do
-  --         y <- input Public :: Comp (UInt 8)
-  --         return $ 1 * (-y)
-  --   -- debug (Binary 7) program 
-  --   check (Binary 7) program [10] [] [246]
   describe "Ordinary Multiplication" $ do
     it "Byte" $ do
       property $ \(a, b) -> do
@@ -61,129 +55,131 @@ tests = describe "Experiment" $ do
             check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
             check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
 
--- describe "Variable-width Multiplication" $ do
---   it "Byte -> UInt 6" $ do
---     property $ \(a, b) -> do
---       let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 64]
---       case (a, b) of
---         (Constant valA, Constant valB) -> do
---           let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 6)
---           check gf181 program [] [] expected
---           check (Prime 17) program [] [] expected
---           check (Binary 7) program [] [] expected
---         (Constant valA, _) -> do
---           let program = do
---                 y <- input Public :: Comp (UInt 8)
---                 return $ fromIntegral valA `mulV` y :: Comp (UInt 6)
---           check gf181 program [operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger b] [] expected
---         (_, Constant valB) -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` fromIntegral valB :: Comp (UInt 6)
---           check gf181 program [operandToUnsignedInteger a] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a] [] expected
---         _ -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 y <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` y :: Comp (UInt 6)
---           check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---   it "Byte -> UInt 8" $ do
---     property $ \(a, b) -> do
---       let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 256]
---       case (a, b) of
---         (Constant valA, Constant valB) -> do
---           let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 8)
---           check gf181 program [] [] expected
---           check (Prime 17) program [] [] expected
---           check (Binary 7) program [] [] expected
---         (Constant valA, _) -> do
---           let program = do
---                 y <- input Public :: Comp (UInt 8)
---                 return $ fromIntegral valA `mulV` y :: Comp (UInt 8)
---           check gf181 program [operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger b] [] expected
---         (_, Constant valB) -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` fromIntegral valB :: Comp (UInt 8)
---           check gf181 program [operandToUnsignedInteger a] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a] [] expected
---         _ -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 y <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` y :: Comp (UInt 8)
---           check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---   it "Byte -> UInt 10" $ do
---     property $ \(a, b) -> do
---       let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 1024]
---       case (a, b) of
---         (Constant valA, Constant valB) -> do
---           let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 10)
---           check gf181 program [] [] expected
---           check (Prime 17) program [] [] expected
---           check (Binary 7) program [] [] expected
---         (Constant valA, _) -> do
---           let program = do
---                 y <- input Public :: Comp (UInt 8)
---                 return $ fromIntegral valA `mulV` y :: Comp (UInt 10)
---           check gf181 program [operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger b] [] expected
---         (_, Constant valB) -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` fromIntegral valB :: Comp (UInt 10)
---           check gf181 program [operandToUnsignedInteger a] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a] [] expected
---         _ -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 y <- input Public :: Comp (UInt 8)
---                 return $ x `mulV` y :: Comp (UInt 10)
---           check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
--- describe "Double-width Multiplication" $ do
---   it "UInt 8 -> UInt 16" $ do
---     property $ \(a, b) -> do
---       let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 65536]
---       case (a, b) of
---         (Constant valA, Constant valB) -> do
---           let program = return $ (fromIntegral valA :: UInt 8) `mulD` fromIntegral valB :: Comp (UInt 16)
---           check gf181 program [] [] expected
---           check (Prime 17) program [] [] expected
---           check (Binary 7) program [] [] expected
---         (Constant valA, _) -> do
---           let program = do
---                 y <- input Public :: Comp (UInt 8)
---                 return $ fromIntegral valA `mulD` y :: Comp (UInt 16)
---           check gf181 program [operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger b] [] expected
---         (_, Constant valB) -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 return $ x `mulD` fromIntegral valB :: Comp (UInt 16)
---           check gf181 program [operandToUnsignedInteger a] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a] [] expected
---         _ -> do
---           let program = do
---                 x <- input Public :: Comp (UInt 8)
---                 y <- input Public :: Comp (UInt 8)
---                 return $ x `mulD` y :: Comp (UInt 16)
---           check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
---           check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+  describe "Variable-width Multiplication" $ do
+    it "Byte -> UInt 6" $ do
+      property $ \(a, b) -> do
+        let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 64]
+        case (a, b) of
+          (Constant valA, Constant valB) -> do
+            let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 6)
+            check gf181 program [] [] expected
+            check (Prime 17) program [] [] expected
+            check (Binary 7) program [] [] expected
+          (Constant valA, _) -> do
+            let program = do
+                  y <- input Public :: Comp (UInt 8)
+                  return $ fromIntegral valA `mulV` operandToSignedVariable b y :: Comp (UInt 6)
+            check gf181 program [operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger b] [] expected
+          (_, Constant valB) -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` fromIntegral valB :: Comp (UInt 6)
+            check gf181 program [operandToUnsignedInteger a] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a] [] expected
+          _ -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  y <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` operandToSignedVariable b y :: Comp (UInt 6)
+            check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+
+    it "Byte -> UInt 8" $ do
+      property $ \(a, b) -> do
+        let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 256]
+        case (a, b) of
+          (Constant valA, Constant valB) -> do
+            let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 8)
+            check gf181 program [] [] expected
+            check (Prime 17) program [] [] expected
+            check (Binary 7) program [] [] expected
+          (Constant valA, _) -> do
+            let program = do
+                  y <- input Public :: Comp (UInt 8)
+                  return $ fromIntegral valA `mulV` operandToSignedVariable b y :: Comp (UInt 8)
+            check gf181 program [operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger b] [] expected
+          (_, Constant valB) -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` fromIntegral valB :: Comp (UInt 8)
+            check gf181 program [operandToUnsignedInteger a] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a] [] expected
+          _ -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  y <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` operandToSignedVariable b y :: Comp (UInt 8)
+            check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+
+    it "Byte -> UInt 10" $ do
+      property $ \(a, b) -> do
+        let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 1024]
+        case (a, b) of
+          (Constant valA, Constant valB) -> do
+            let program = return $ (fromIntegral valA :: UInt 8) `mulV` fromIntegral valB :: Comp (UInt 10)
+            check gf181 program [] [] expected
+            check (Prime 17) program [] [] expected
+            check (Binary 7) program [] [] expected
+          (Constant valA, _) -> do
+            let program = do
+                  y <- input Public :: Comp (UInt 8)
+                  return $ fromIntegral valA `mulV` operandToSignedVariable b y :: Comp (UInt 10)
+            check gf181 program [operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger b] [] expected
+          (_, Constant valB) -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` fromIntegral valB :: Comp (UInt 10)
+            check gf181 program [operandToUnsignedInteger a] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a] [] expected
+          _ -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  y <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulV` operandToSignedVariable b y :: Comp (UInt 10)
+            check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+  describe "Double-width Multiplication" $ do
+    it "UInt 8 -> UInt 16" $ do
+      property $ \(a, b) -> do
+        let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 65536]
+        case (a, b) of
+          (Constant valA, Constant valB) -> do
+            let program = return $ (fromIntegral valA :: UInt 8) `mulD` fromIntegral valB :: Comp (UInt 16)
+            check gf181 program [] [] expected
+            check (Prime 17) program [] [] expected
+            check (Binary 7) program [] [] expected
+          (Constant valA, _) -> do
+            let program = do
+                  y <- input Public :: Comp (UInt 8)
+                  return $ fromIntegral valA `mulD` operandToSignedVariable b y :: Comp (UInt 16)
+            check gf181 program [operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger b] [] expected
+          (_, Constant valB) -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulD` fromIntegral valB :: Comp (UInt 16)
+            check gf181 program [operandToUnsignedInteger a] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a] [] expected
+          _ -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  y <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x `mulD` operandToSignedVariable b y :: Comp (UInt 16)
+            check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
