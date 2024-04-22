5e8d97906fd9f6dfca9924bb0d7b09a3928478e8
Ting-gian LUA <banacorn@gmail.com>
2024-04-22 16:17:26 +0800

    [ fix ] Remove RefUBits from Compile.UInt.Multiplication.Binary

--- src/Keelung/Compiler/Compile/UInt/Multiplication/Binary.hs
+++ src/Keelung/Compiler/Compile/UInt/Multiplication/Binary.hs
@@ -7,6 +7,7 @@ import Keelung (Width, widthOf)
 import Keelung.Compiler.Compile.Monad
 import Keelung.Data.Reference
 import Keelung.Data.Slice (Slice (Slice))
+import Keelung.Data.Slice qualified as Slice
 import Keelung.Data.U (U)
 import Keelung.Data.U qualified as U
 
@@ -50,18 +51,18 @@ compile out refX operandY = do
 --                      x₃y₁    x₂y₁    x₁y₁    x₀y₁
 --              x₃y₂    x₂y₂    x₁y₂    x₀y₂
 --      x₃y₃    x₂y₃    x₁y₃    x₀y₃
-formAllColumns :: (GaloisField n, Integral n) => Width -> RefU -> Either RefU U -> M n [(Int, [RefB])]
+formAllColumns :: (GaloisField n, Integral n) => Width -> RefU -> Either RefU U -> M n [(Int, [RefUBit])]
 formAllColumns productWidth x (Left y) = do
   let width = widthOf x
-  let pairs = [(indexSum, [(RefUBit x (indexSum - i), RefUBit y i) | i <- [(indexSum - width + 1) `max` 0 .. indexSum `min` (width - 1)]]) | indexSum <- [0 .. productWidth - 1]]
+  let pairs = [(indexSum, [((x, indexSum - i), (y, i)) | i <- [(indexSum - width + 1) `max` 0 .. indexSum `min` (width - 1)]]) | indexSum <- [0 .. productWidth - 1]]
   forM pairs $ \(i, xs) -> do
-    xs' <- mapM multiplyBits xs
+    xs' <- mapM multiplyBitsAlloc xs
     return (i, xs')
 formAllColumns productWidth x (Right y) = do
   let width = widthOf x
   forM [0 .. productWidth - 1] $ \i -> do
     -- put the bits of x into the column if `Data.Bits.testBit y j` is True
-    let vars = [RefUBit x (i - j) | j <- [(i - width + 1) `max` 0 .. i `min` (width - 1)], Data.Bits.testBit y j]
+    let vars = [(x, i - j) | j <- [(i - width + 1) `max` 0 .. i `min` (width - 1)], Data.Bits.testBit y j]
     return (i, vars)
 
 -- | Form only the right half of columns of bits to be added together after multiplication
@@ -73,31 +74,35 @@ formAllColumns productWidth x (Right y) = do
 --      x₂y₁    x₁y₁    x₀y₁
 --      x₁y₂    x₀y₂
 --      x₀y₃
-formHalfColumns :: (GaloisField n, Integral n) => Width -> RefU -> Either RefU U -> M n [(Int, [RefB])]
+formHalfColumns :: (GaloisField n, Integral n) => Width -> RefU -> Either RefU U -> M n [(Int, [RefUBit])]
 formHalfColumns width x (Left y) = do
   let numberOfColumns = width
   forM [0 .. numberOfColumns - 1] $ \i -> do
-    let pairs = [(RefUBit x (i - j), RefUBit y j) | j <- [0 `max` 0 .. i]]
-    xs' <- mapM multiplyBits pairs
+    let pairs = [((x, i - j), (y, j)) | j <- [0 `max` 0 .. i]]
+    xs' <- mapM multiplyBitsAlloc pairs
     return (i, xs')
 formHalfColumns width x (Right y) = do
   let numberOfColumns = width
   forM [0 .. numberOfColumns - 1] $ \i -> do
     -- put the bits of x into the column if `Data.Bits.testBit y j` is True
-    let vars = [RefUBit x (i - j) | j <- [0 `max` 0 .. i], Data.Bits.testBit y j]
+    let vars = [(x, i - j) | j <- [0 `max` 0 .. i], Data.Bits.testBit y j]
     return (i, vars)
 
 -- | Starting from the least significant column, add the bits together and propagate the carry to the next column
-foldColumns :: (GaloisField n, Integral n) => RefU -> [(Int, [RefB])] -> M n ()
+foldColumns :: (GaloisField n, Integral n) => RefU -> [(Int, [RefUBit])] -> M n ()
 foldColumns out = foldM_ step []
   where
-    step :: (GaloisField n, Integral n) => [RefB] -> (Int, [RefB]) -> M n [RefB]
+    step :: (GaloisField n, Integral n) => [RefUBit] -> (Int, [RefUBit]) -> M n [RefUBit]
     step prevCarries (columnIndex, column)
       | widthOf out <= columnIndex = return [] -- out of the range of `out`
-      | otherwise = foldColumn (RefUBit out columnIndex) (prevCarries <> column)
+      | otherwise = do
+          let column' = prevCarries <> column
+          foldColumn (out, columnIndex) column'
+
+----------------------------------------
 
 -- | Add up a column of bits and propagate the carry to the next column
-foldColumn :: (GaloisField n, Integral n) => RefB -> [RefB] -> M n [RefB]
+foldColumn :: (GaloisField n, Integral n) => RefUBit -> [RefUBit] -> M n [RefUBit]
 foldColumn out xs = do
   let (chunk, rest) = splitAt 3 xs
   if null rest
@@ -106,55 +111,58 @@ foldColumn out xs = do
       combineBits out chunk
     else do
       -- handle the chunk
-      tempOut <- freshRefB
+      tempOut <- do
+        slice <- allocSlice 1
+        return (Slice.sliceRefU slice, 0)
       carry <- combineBits tempOut chunk
       -- push the result of the chunk back to the stack
       carries <- foldColumn out (tempOut : rest)
       return $ carry <> carries
 
 -- | Add up a limited number of bits and return the carry
-combineBits :: (GaloisField n, Integral n) => RefB -> [RefB] -> M n [RefB]
-combineBits out [] = do
-  writeRefVal (B out) 0
+combineBits :: (GaloisField n, Integral n) => RefUBit -> [RefUBit] -> M n [RefUBit]
+combineBits (out, i) [] = do
+  writeSliceVal (Slice out i (i + 1)) 0
   return [] -- no carry
 combineBits out [a] = do
-  addBits' out [a]
+  addBits out [a]
   return [] -- no carry
 combineBits out [a, b] = do
+  -- bit allocation: 1
   -- out[i] = a + b
-  addBits' out [a, b]
+  addBits out [a, b]
   -- carry = a * b
-  carry <- multiplyBits (a, b)
+  carry <- multiplyBitsAlloc (a, b)
   return [carry]
 combineBits out [a, b, c] = do
+  -- bit allocation: 4
   -- out[i] = a + b + c
-  addBits' out [a, b, c]
+  addBits out [a, b, c]
   -- carry = a * b + a * c + b * c
-  ab <- multiplyBits (a, b)
-  ac <- multiplyBits (a, c)
-  bc <- multiplyBits (b, c)
-  carry <- addBits [ab, ac, bc]
+  ab <- multiplyBitsAlloc (a, b)
+  ac <- multiplyBitsAlloc (a, c)
+  bc <- multiplyBitsAlloc (b, c)
+  carry <- addBitsAlloc [ab, ac, bc]
   return [carry]
 combineBits _ _ = error "[ panic ] combineBits: cannot handle too many bits"
 
--- | Add up a list of bits
-addBits :: (GaloisField n, Integral n) => [RefB] -> M n RefB
-addBits xs = do
-  out <- freshRefB
-  writeAdd 0 ((B out, -1) : [(B x, 1) | x <- xs]) []
-  return out
+type RefUBit = (RefU, Int)
 
--- | Like `addBits` but write the result to an existing reference
-addBits' :: (GaloisField n, Integral n) => RefB -> [RefB] -> M n ()
-addBits' out xs = writeAdd 0 ((B out, -1) : [(B x, 1) | x <- xs]) []
+-- | Add up a list of bits
+addBitsAlloc :: (GaloisField n, Integral n) => [RefUBit] -> M n RefUBit
+addBitsAlloc xs = do
+  out <- allocSlice 1
+  writeAdd 0 [] ((out, -1) : [(Slice x i (i + 1), 1) | (x, i) <- xs])
+  return (Slice.sliceRefU out, 0)
+
+-- | Like `addBitsAlloc` but write the result to an existing reference
+addBits :: (GaloisField n, Integral n) => RefUBit -> [RefUBit] -> M n ()
+addBits (out, i) xs = writeAdd 0 [] ((Slice out i (i + 1), -1) : [(Slice x j (j + 1), 1) | (x, j) <- xs])
 
 -- | Multiply two bits
-multiplyBits :: (GaloisField n, Integral n) => (RefB, RefB) -> M n RefB
-multiplyBits (x, y) = do
-  out <- freshRefB
-  multiplyBits' out (x, y)
-  return out
-
--- | Like `multiplyBits` but write the result to an existing reference
-multiplyBits' :: (GaloisField n, Integral n) => RefB -> (RefB, RefB) -> M n ()
-multiplyBits' out (x, y) = writeMul (0, [(B x, 1)], []) (0, [(B y, 1)], []) (0, [(B out, 1)], [])
+multiplyBitsAlloc :: (GaloisField n, Integral n) => (RefUBit, RefUBit) -> M n RefUBit
+multiplyBitsAlloc ((x, i), (y, j)) = do
+  -- out <- freshRefB
+  out <- allocSlice 1
+  writeMul (0, [], [(Slice x i (i + 1), 1)]) (0, [], [(Slice y j (j + 1), 1)]) (0, [], [(out, 1)])
+  return (Slice.sliceRefU out, 0)
--- test/Test/Compilation/Experiment.hs
+++ test/Test/Compilation/Experiment.hs
@@ -22,44 +22,44 @@ run = hspec tests
 
 tests :: SpecWith ()
 tests = describe "Experiment" $ do
-  it "failed" $ do
-    let program = do
-          y <- input Public :: Comp (UInt 8)
-          return $ 1 * (-y)
-    -- debug (Binary 7) program 
-    check (Binary 7) program [10] [] [246]
-  -- describe "Ordinary Multiplication" $ do
-  --   it "Byte" $ do
-  --     property $ \(a, b) -> do
-  --       let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 256]
-  --       case (a, b) of
-  --         (Constant valA, Constant valB) -> do
-  --           let program = return $ fromIntegral valA * fromIntegral valB :: Comp (UInt 8)
-  --           check gf181 program [] [] expected
-  --           check (Prime 17) program [] [] expected
-  --           check (Binary 7) program [] [] expected
-  --         (Constant valA, _) -> do
-  --           let program = do
-  --                 y <- input Public :: Comp (UInt 8)
-  --                 return $ fromIntegral valA * operandToSignedVariable b y
-  --           check gf181 program [operandToUnsignedInteger b] [] expected
-  --           check (Prime 17) program [operandToUnsignedInteger b] [] expected
-  --         -- check (Binary 7) program [operandToUnsignedInteger b] [] expected
-  --         (_, Constant valB) -> do
-  --           let program = do
-  --                 x <- input Public :: Comp (UInt 8)
-  --                 return $ operandToSignedVariable a x * fromIntegral valB
-  --           check gf181 program [operandToUnsignedInteger a] [] expected
-  --           check (Prime 17) program [operandToUnsignedInteger a] [] expected
-  --         -- check (Binary 7) program [operandToUnsignedInteger a] [] expected
-  --         _ -> do
-  --           let program = do
-  --                 x <- input Public :: Comp (UInt 8)
-  --                 y <- input Public :: Comp (UInt 8)
-  --                 return $ operandToSignedVariable a x * operandToSignedVariable b y
-  --           check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
-  --           check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
-  --           check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+  -- it "failed" $ do
+  --   let program = do
+  --         y <- input Public :: Comp (UInt 8)
+  --         return $ 1 * (-y)
+  --   -- debug (Binary 7) program 
+  --   check (Binary 7) program [10] [] [246]
+  describe "Ordinary Multiplication" $ do
+    it "Byte" $ do
+      property $ \(a, b) -> do
+        let expected = [(operandToSignedInteger a * operandToSignedInteger b) `mod` 256]
+        case (a, b) of
+          (Constant valA, Constant valB) -> do
+            let program = return $ fromIntegral valA * fromIntegral valB :: Comp (UInt 8)
+            check gf181 program [] [] expected
+            check (Prime 17) program [] [] expected
+            check (Binary 7) program [] [] expected
+          (Constant valA, _) -> do
+            let program = do
+                  y <- input Public :: Comp (UInt 8)
+                  return $ fromIntegral valA * operandToSignedVariable b y
+            check gf181 program [operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger b] [] expected
+          (_, Constant valB) -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x * fromIntegral valB
+            check gf181 program [operandToUnsignedInteger a] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a] [] expected
+          _ -> do
+            let program = do
+                  x <- input Public :: Comp (UInt 8)
+                  y <- input Public :: Comp (UInt 8)
+                  return $ operandToSignedVariable a x * operandToSignedVariable b y
+            check gf181 program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Prime 17) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
+            check (Binary 7) program [operandToUnsignedInteger a, operandToUnsignedInteger b] [] expected
 
 -- describe "Variable-width Multiplication" $ do
 --   it "Byte -> UInt 6" $ do
