5b6342823b6536f9458b60a81e7289a28c99ea17
Ting-gian LUA <banacorn@gmail.com>
2024-05-15 14:03:12 +0800

    [ new ] Property testing for solving satisfiable polynomials on binary fields

--- test/Test/Solver/Binary.hs
+++ test/Test/Solver/Binary.hs
@@ -4,11 +4,13 @@
 module Test.Solver.Binary (tests, run) where
 
 import Data.Field.Galois
+import Data.IntMap (IntMap)
 import Data.IntMap qualified as IntMap
 import Keelung.Data.Polynomial (Poly)
 import Keelung.Data.Polynomial qualified as Poly
 import Keelung.Solver.Binary qualified as Binary
 import Test.Hspec
+import Test.QuickCheck
 
 run :: IO ()
 run = hspec tests
@@ -65,3 +67,27 @@ tests = describe "Binary" $ do
     let actual = Binary.run polynomial
     let expected = Just (IntMap.fromList [(2, True)], [remaining])
     actual `shouldBe` expected
+
+  describe "satisfiable" $ do
+    it "Binary 283" $ do
+      property $ \(Satisfiable polynomial assignments) -> do
+        let actual = Binary.run (polynomial :: Poly (Binary 283))
+        let expected = Just (assignments, [])
+        actual `shouldBe` expected
+
+-------------------------------------------------------------------------------
+
+instance (GaloisField n, Arbitrary n) => Arbitrary (TestCase n) where
+  arbitrary = do
+    numberOfTerms <- choose (1, 2)
+    coefficients <- vectorOf numberOfTerms (arbitrary `suchThat` (> 0)) :: Gen [n]
+
+    assignments <- vectorOf numberOfTerms arbitrary
+    let constant = sum $ zipWith (\coeff val -> if val then coeff else 0) coefficients assignments
+    let polynomial = case Poly.buildEither constant (zip [0 ..] coefficients) of
+          Left _ -> error "Poly.buildEither"
+          Right p -> p :: Poly n
+    pure $ Satisfiable polynomial (IntMap.fromDistinctAscList $ zip [0 ..] assignments)
+
+data TestCase n = Satisfiable (Poly n) (IntMap Bool)
+    deriving (Show)
